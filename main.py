2d52ec7cee22aa3e51531a4c172b53d18a5ab63e
zzampax <zpx171997@gmail.com>
2025-02-23 12:50:20 +0100

    Fix PDF's link retrieval
    Improve verbosity, prefer bs4 to regex

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import os
-import time
+import re
+import sys
 import base64
 import requests
-import re
+import time
+from datetime import datetime
+from bs4 import BeautifulSoup as BS
 
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
@@ -16,8 +19,8 @@ SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"]
 
 def set_last_message_id():
     # Get the last message ID from the PDFs folder
-    if os.path.exists("kindle_pdfs"):
-        pdfs = os.listdir("kindle_pdfs")
+    if os.path.exists("kindle-pdfs"):
+        pdfs = os.listdir("kindle-pdfs")
         if pdfs:
             pdfs.sort(key=lambda x: float(x.split("_")[1]))
             return pdfs[-1].split("_")[2].split(".")[0]
@@ -32,11 +35,12 @@ def auth():
         creds = Credentials.from_authorized_user_file("token.json", SCOPES)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
-        print("No valid credentials")
+        print(f"[*] No valid token.json found...")
         if creds and creds.expired and creds.refresh_token:
-            print("Refreshing credentials, please wait...")
+            print("[*] Refreshing credentials, please wait...")
             creds.refresh(Request())
         else:
+            print()
             flow = InstalledAppFlow.from_client_secrets_file(
                 "credentials.json", SCOPES
             )
@@ -48,9 +52,13 @@ def auth():
 
 def fetch_pdf(pdf_link, filename):
     # Download the PDF
-    response = requests.get(pdf_link)
-    with open(f"kindle_pdfs/{filename}.pdf", "wb") as pdf:
-        pdf.write(response.content)
+    try:
+        response = requests.get(pdf_link)
+        with open(f"kindle-pdfs/{filename}.pdf", "wb") as pdf:
+            pdf.write(response.content)
+    except Exception as err:
+        print("[!] Something went wrong while downloading {filename}.pdf:\n{err}")
+        sys.exit(2)
 
 def main(creds):
     global last_message_id
@@ -73,24 +81,30 @@ def main(creds):
             .execute()
         )
 
+
         # If the last message is the same as the previous one, return
+        if last_message_id == None:
+            last_message_id = results["messages"][0]["id"]
+            return
         if last_message_id == results["messages"][0]["id"]:
             return
         last_message_id = results["messages"][0]["id"]
+        print("[*] Found new Kindle PDF, now downloading...")
 
-        # Get the content of the email
-        b64content = "".join(part["body"]["data"] for part in message["payload"]["parts"] if part["mimeType"] == "text/plain")
+        b64content = "".join(part["body"]["data"] for part in message["payload"]["parts"] if part["mimeType"] == "text/html")
         content = base64.urlsafe_b64decode(b64content).decode("utf-8")
-        # Get the link to the PDF
-        pdf_link = [link for link in re.findall(r'\((https?://[^\s)]+)\)', content) if "kindle" in link]
-        if not pdf_link:
+        soup = BS(content, "html.parser")
+        anchor = [a for a in soup.find_all('a') if "Download PDF" == a.text]
+
+        if len(anchor) != 1:
             return
-        pdf_link = pdf_link[0]
+
+        pdf_link = str(anchor[0].get("href"))
         # Set the filename to the current time and the message ID
         filename = f"{message["snippet"].split("&quot;")[1].replace(" ", "-")}_{time.time()}_{results['messages'][0]['id']}"
         # Download the PDF
         fetch_pdf(pdf_link, filename)
-        print(f"Downloaded {filename.split('_')[0]}.pdf")
+        print(f"[+] Downloaded to kindle-pdfs/{filename}.pdf")
 
     except HttpError as error:
         # TODO(developer) - Handle errors from gmail API.
@@ -98,8 +112,18 @@ def main(creds):
 
 
 if __name__ == "__main__":
+    print(f"[*] Checking OAuth2.0 Modules...")
+    if not os.path.isfile("credentials.json"):
+        print(f"[!] Credentials file missing, if present rename it to credentails.json an place it in this directory")
+        sys.exit(1)
     creds = auth()
+
+    if not os.path.exists("kindle-pdfs"):
+        os.mkdir("kindle-pdfs")
+
     last_message_id = set_last_message_id()
     while True:
+        now = datetime.now()
+        print(f"[*] Checking GMail inbox at {now.strftime('%Y-%m-%d %H:%M:%S')}...")
         main(creds)
         time.sleep(2)
